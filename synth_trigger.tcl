3a0374abbe0fe73677414c5e2d834bf04df2addf
David@TU <geht@dinix.aun>
2018-05-09 09:49:03 +0200

    trigger.vhdl added

---
+++ synth_trigger.tcl
@@ -0,0 +1,41 @@
+#------------------------------------------------------------------------------
+#
+# Synthesis script for TRNG using Digilent Nexys 4 DDR board
+#
+# -----------------------------------------------------------------------------
+#
+create_project -part xc7a100t -force trigger
+#
+# -----------------------------------------------------------------------------
+#
+
+#read_vhdl postprocessor.vhdl
+#read_vhdl rng1.vhdl
+#read_vhdl noise1.vhdl -vhdl2008
+#read_vhdl SEPA.vhdl
+#read_vhdl uart_tx.vhd
+#read_vhdl main.vhdl
+#read_vhdl 7seg.vhdl
+#read_vhdl 7display.vhdl
+read_vhdl trigger.vhdl
+
+read_xdc  main.xdc
+
+#
+# -----------------------------------------------------------------------------
+#
+synth_design -top trigger
+#
+# -----------------------------------------------------------------------------
+#
+
+opt_design
+place_design
+route_design
+
+#
+# -----------------------------------------------------------------------------
+#
+#write_bitstream -force main.bit
+#
+# -----------------------------------------------------------------------------

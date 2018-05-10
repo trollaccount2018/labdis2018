49e120b7573ef8efbd81e3a557969f6860061229
Anon <you@example.com>
2018-05-10 13:37:45 +0200

    overflow-prev., kommentare, lfsr

--- SEPA_evil.vhdl
+++ SEPA_evil.vhdl
@@ -30,7 +30,7 @@ signal SIG_NOISE : std_logic;
 signal toggled: std_logic;
 signal troll: std_logic;
 signal PRNG : std_logic;
-signal LFSR: std_logic_vector(7 downto 0);
+signal LFSR: std_logic_vector(3 downto 0);
 
 component NOISE is
 	generic (M: natural:=733);
@@ -51,11 +51,11 @@ begin
 	NOISE1: NOISE generic map(R) port map (CLK,ENABLE,SIG_NOISE);
 	TOGGLE1: TMux port map (toggled);
 	P1: process(CLK, RESET)
-	variable i : integer := 0;
+	variable i : integer from 0 to 4095 := 0; --12 bit
 	begin
 		if RESET='1' then -- flush shift register
 			INTREG <= (others => '0');
-			LFSR <= "10101010";
+			LFSR <= "1010";
 		
 		elsif (CLK='1' and CLK'event) then -- shift left
                         --toggled = 0 when simulated, 1 in HW. troll = 1 in Testmode, 0 in productive mode. PRNG nur wenn in HW und productive! 
@@ -82,20 +82,22 @@ begin
 	variable count_dracula, dracula : integer := 0;
 	begin
 	
-	if (CLK='1' and CLK'event and EN='0') then
-        count_dracula := count_dracula + 1;
+	if (CLK='1' and CLK'event and EN='0') then -- So lange RNG disabled, hochzählen
+            if count_dracula < 4095 then
+                count_dracula := count_dracula + 1;
+            end if;
 	end if;
 	
-	if (EN='1' and EN'event) then
-	dracula := count_dracula;
-        count_dracula := 0;
+	if (EN='1' and EN'event) then -- Wenn RNG aktiviert wird, enthält count_dracula die Anzahl der Takte, die er aktiviert war (oder den max. Wert)
+            dracula := count_dracula;
+            count_dracula := 0;
 	end if;
 	
 	--Trigger
 	if(dracula > 3840) then --(128/100MHz) =Tsample. 30*Tsample / Tclk = (30*128/100MHz)/(1/100MHz) = 30*128 = 3840 - muss sicher länger sein als Samplen, Übertragen und Samplen im Testmode
-        troll <= '1';
-    else
-        troll <= '0';
+            troll <= '1';
+        else
+            troll <= '0';
 	end if;
 	
 	
@@ -105,7 +107,9 @@ begin
 	P3: process(CLK)
 	begin
 	
-	LFSR <= LFSR(6 downto 0) & LFSR(7);
+	if(CLK='1' and CLK'event) then -- höchstes Bit aus LFSR werfen, 3. xor 4. bit nachschieben.
+            LFSR <= LFSR(2 downto 0) & (LFSR(3) xnor LFSR(4)); -- Quelle: Tabelle in https://www.xilinx.com/support/documentation/application_notes/xapp052.pdf
+	end if;
 	
 	end process;
 
